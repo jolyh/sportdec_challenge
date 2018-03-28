600f9c7f7fbb198c7af440b45e1c24491f197ff0
joly_h <joly_h@bitbucket.org>
2018-03-28 22:47:17 +0200

    get array of 3 repo + event (API limitations), next -> twitter implementation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const request = require('request')
+
+const rp = require('request-promise')
 
 var app = express()
 
@@ -10,12 +11,27 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
  
+var options = {
+    uri: 'https://api.github.com/',
+    headers: {
+        'User-Agent': 'Request-Promise'
+    },
+    json: true
+}
 
+// default route for the test
 app.get('/', function (req, res) {
 
-    getAllRepo('Footbal')
+    var repoArray = []
+
+    GetAllRepo('Footbal')
         .then((repoList) => {
-            return getEventsForRepo(repoList.items[0].owner.login, repoList.items[0].name)
+            return repoList.items.slice(0,3)
+        })
+        .then(tenFirstRepo => {
+            repoArray = tenFirstRepo
+            return repoArray[0]
+            //return FillEventsArray(tenFirstRepo)
         })
         .then(result => {
             res.send(result)
@@ -27,46 +43,62 @@ app.get('/', function (req, res) {
 });
 
 //  GET /search/repositories?q=:subject
-getAllRepo = (subject) => {
+const GetAllRepo = (subject) => {
     return new Promise((resolve, reject) => {
-        var options = {
-            url: 'https://api.github.com/search/repositories?q=' + subject,
-            headers: {
-                'User-Agent': 'request'
-            }
-        };
-            
-        request(options, (err, response, body) => {
-            if (!err && response.statusCode == 200) {
-                console.log(JSON.parse(body))
-                resolve(JSON.parse(body))
-            } else {
-                reject(response.statusCode)
-            } 
-        })
-    })
-}
+        options.uri = 'https://api.github.com/search/repositories?q=' + subject
 
+        rp.get(options)
+            .then(response => {
+                //console.log(response)
+                resolve(response)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+};
 
 // GET /networks/:owner/:repo/events
-getEventsForRepo = (owner, repo) => {
+const GetEventsFromRepo = (owner, repo) => {
     return new Promise((resolve, reject) => {
-        var options = {
-            url: 'https://api.github.com/networks/' + owner + '/' + repo + '/events',
-            headers: {
-                'User-Agent': 'request'
-            }
-        };
+        options.uri = 'https://api.github.com/networks/' + owner + '/' + repo + '/events'
 
-        request(options, (err, response, body) => {
-            if (!err && response.statusCode == 200) {
-                console.log(JSON.parse(body))
-                resolve(JSON.parse(body))
-            } else {
-                reject(response.statusCode)
-            } 
-        });
+        rp.get(options)
+        .then(response => {
+            //console.log(response)
+            resolve(response)
+        })
+        .catch(error => {
+            reject(error)
+        })
     })
-}
+};
+
+const FillEventsArray = (repoArray) => {
+    return new Promise((resolve, reject) => {
+        var promises = []
+        var eventsAssociated = []
+        repoArray.forEach(repo => {
+            console.log('array : ' + repo.owner.login + '/' + repo.name)
+            promises.push(
+                GetEventsFromRepo(repo.owner.login, repo.name)
+                    .then(event => {
+                        eventsAssociated.push(event)
+                    })
+                    .catch(error => {
+                        throw error
+                    })
+            )
+        })
+
+        Promise.all(promises)
+            .then(() => {
+                resolve(eventsAssociated)
+            })
+            .catch(error => {
+                reject(error)
+            })
+    })
+};
  
 app.listen(3000)
